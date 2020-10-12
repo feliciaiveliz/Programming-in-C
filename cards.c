9b5fc8105609ab2f45d1e4619fca512bb8bed027
feliciaiveliz <feliciaiveliz@gmail.com>
2020-10-12 12:57:32 -0500

    Adding more C exercises

--- cards.c
+++ cards.c
@@ -1,23 +1,31 @@
+/*
+ * Program to evaluate face values.
+ * Released under the Vegas Public License.
+ * (c)2014 The College Blackjack Team.
+ */
 #include<stdio.h>
 #include<stdlib.h>
-
 int main()
 {
-  char card_name[3];
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
-  int val = 0;
-  if (card_name[0] == 'K') {
-      val = 10;
-  } else if (card_name[0] == 'Q') {
-      val = 10;
-  } else if (card_name[0] == 'J') {
-      val = 10;
-  } else if (card_name[0] == 'A') {
-      val = 11;
-  } else {
-      val = atoi(card_name);
-  }
-  printf("The card value is: %i\n", val);
-  return 0;
+    char card_name[3]; /* Initializing an array 'card_name' to only have 3 elements (last element is \o) */
+    puts("Enter the card_name: "); /* displays this string */
+    scanf("%2s", card_name); /* ? */
+    int val = 0; /* initializing an integer variable 'val' to 0 */
+    if (card_name[0] == 'K') { /* start of 'if' statement. if the first element is equal to 'K'... then enter blocks statement */
+        val = 10;
+    } else if (card_name[0] == 'Q') {
+        val = 10;
+    } else if (card_name[0] == 'J') {
+        val = 10;
+    } else if (card_name[0] == 'A') {
+        val = 11;
+    } else {
+        val = atoi(card_name);
+    }
+
+    if ((val > 2) && (val < 7))
+        puts("Count has gone up!");
+    else if (val == 10)
+        puts("Count has gone down!");
+    return 0; /* the program should return 0 if it was successful */
 }
---
+++ for-loops.c
@@ -0,0 +1,4 @@
+int counter;
+for (counter = 1; counter < 11; counter++) {
+  printf("%i green bottles hanging on the wall\n", counter);
+}
