2136cdee00d6bfcda86ffd12ba7e76c767629878
feliciaiveliz <feliciaiveliz@gmail.com>
2020-10-16 12:57:03 -0500

    Revising strings file

--- strings.c
+++ strings.c
@@ -4,10 +4,12 @@
 int main()
 {
   void fortune_cookie(char msg[])
-{
-  printf("Message reads: %s\n", msg);
+  {
+    printf("Message reads: %s\n", msg);
+    printf("msg occupies %i bytes\n", sizeof(msg));
+    printf("The quote string is stored at: %p\n", msg);
+  }
+  char quote[] = "Life is short, eat the cookie.";
+  fortune_cookie(quote);
 }
 
-char quote[] = "Life is short, eat the cookie.";
-fortune_cookie(quote);
-}
